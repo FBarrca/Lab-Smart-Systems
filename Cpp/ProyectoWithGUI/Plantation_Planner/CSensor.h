1cb55dcbcd5ed6641c3964c8a0f1abae3e8f3506
lorenzo-rod <66308571+lorenzo-rod@users.noreply.github.com>
2022-11-15 16:12:25 +0100

    updated CSensor.h

--- Cpp/ProyectoWithGUI/Plantation_Planner/CSensor.h
+++ Cpp/ProyectoWithGUI/Plantation_Planner/CSensor.h
@@ -1,16 +1,24 @@
 #pragma once
 #include <vector>
-#include "CMeasurement.h"
+#include "CValue.h"
 
 class CSensor
 {
 private:
-	std::vector<std::shared_ptr<CMeasurement>> sensor_measurements;
+	std::vector<CValue*> sensor_values;
 	int id_sensor;
 public:
+	// constructor and destructor
 	CSensor();
-	CSensor(int id, std::vector<std::shared_ptr<CMeasurement>> measurements);
+	CSensor(int id, std::vector<CValue*> values);
 	~CSensor();
 
+	// getters and setters
+	int getID() const;
+	void getValues(std::vector<CValue*>& values);
+	CValue getLastnValue(int n);
+	CValue getLastValue() { return getLastnValue(1); };
+	void setID(int id);
+	void setValues(std::vector<CValue*> values);
 
 };
