f83080e0667d889b594bc0c9ab84791a84c5955c
FranciscoBarCa <60709314+FranciscoBarCa@users.noreply.github.com>
2022-11-17 16:21:45 +0100

    Clean duplicate CSensor

--- Cpp/ProyectoWithGUI/Plantation_Planner/bananas/CSensor.cpp
+++ Cpp/ProyectoWithGUI/Plantation_Planner/bananas/CSensor.cpp
@@ -1,1 +1,93 @@
 #include "CSensor.h"
+
+CSensor::CSensor()
+{
+	std::vector<CValue*> values;
+	id_sensor = 0;
+	sensor_values = values;
+
+}
+
+CSensor::CSensor(int id, std::vector<CValue*> values)
+{
+	id_sensor = id;
+	sensor_values = values;
+}
+
+CSensor::~CSensor()
+{
+}
+
+int CSensor::getID() const
+{
+	return id_sensor;
+}
+
+void CSensor::getValues(std::vector<CValue*>& values)
+{
+	std::vector<CValue*>::iterator it;
+	it = sensor_values.begin();
+	for (int i = 0; i < sensor_values.size(); i++, it++)
+	{
+		values.push_back(*it);
+	}
+}
+
+//void CSensor::getLastnValue(int n, std::vector<CValue>& values)
+//{
+//	
+//}
+
+CValue CSensor::getLastValue()
+{
+	std::vector<CValue*>::iterator it;
+	it = sensor_values.begin();
+	time_t min_t = 0;
+	CValue lastValue((*it)->getValue(), (*it)->getDate());
+	for (int i = 0; i < sensor_values.size(); i++, it++)
+	{
+		if ((lastValue.getDate()) < ((*it)->getDate()))
+		{
+			lastValue.setDate((*it)->getDate());
+			lastValue.setValue((*it)->getValue());
+		}
+	}
+
+	return lastValue;
+
+}
+
+void CSensor::setID(int id)
+{
+	id_sensor = id;
+}
+
+void CSensor::setValues(std::vector<CValue*> Values)
+{
+	std::vector<CValue*>::iterator it;
+	it = Values.begin();
+	for (int i = 0; i < Values.size(); i++, it++)
+	{
+		sensor_values.push_back(*it);
+	}
+}
+
+SensorType::SensorType()
+{
+	m_id = 0;
+	m_description = "";
+	m_unit = "";
+	m_Loc = SECTORTYPE;
+}
+
+SensorType::~SensorType()
+{
+}
+
+SensorType::SensorType(unsigned int id, std::string description, std::string unit, TypeOfLoc Loc)
+{
+	m_id = id;
+	m_description = description;
+	m_unit = unit;
+	m_Loc = Loc;
+}
--- Cpp/ProyectoWithGUI/Plantation_Planner/bananas/CSensor.h
+++ Cpp/ProyectoWithGUI/Plantation_Planner/bananas/CSensor.h
@@ -1,81 +1,47 @@
 #pragma once
-#include <String>
-//#include "CSenseType.h"
-//#include "CMeasurement.h"
 #include <vector>
+#include "bananas/CValue.h"
 
-#include <boost/make_shared.hpp>
+typedef enum {
+	SECTORTYPE,
+	PIPETYPE
+} TypeOfLoc;
 
+class SensorType
+{
+public:
+	SensorType();
+	~SensorType();
+	SensorType(unsigned int id, std::string description, std::string unit, TypeOfLoc Loc);
 
+private:
+	unsigned int m_id;
+	std::string m_description;
+	std::string m_unit;
+	TypeOfLoc m_Loc;
 
-
-/* This is a general class for both PIPE and SECTOR sensors.
-* As they both act the same way it would be absurd to make two classes
-* Each type of sensor has its own child class.
-* The sensor and pipe classes do a type check before adding sensors using the sensor type 
-to prevent adding a sensor at the wrong location type
-*/
+};
 
 class CSensor
 {
+private:
+	std::vector<CValue*> sensor_values;
+	int id_sensor;
+	SensorType m_type;
 public:
+	// constructor and destructor
 	CSensor();
-	CSensor(std::string d, CSenseType st, CMeasType mt) : m_description(d), m_type(st){};
+	CSensor(int id, std::vector<CValue*> values);
 	~CSensor();
 
-	// Creates a measurement and adds it to the vector of measurements.
-	//  [in] values - vector of values to be added to the measurement
-	void createMeasurement(std::vector<CValue> values){
-		//mp_measurements.push_back(boost::make_shared<CMeasurement>(new CMeasurement(values, m_type)));
-	};
-	CSenseType& m_getType() { return m_type; };
-private:
-	std::string m_description;									  // name of sensor
-	CSenseType m_type;										  //
-	bool m_isSwitch;											  // if the sensor is a switch or not(aka % on or off)
-	std::vector<boost::shared_ptr<CMeasurement>> mp_measurements; // vector of measurements over time
+	// getters and setters
+	int getID() const;
+	void getValues(std::vector<CValue*>& values);
+	//void getLastnValue(int n, std::vector<CValue>& values);
+	CValue getLastValue();
+	void setID(int id);
+	void setValues(std::vector<CValue*> values);
+
 };
 
 
-class CFlow : public CSensor
-{
-public:
-	CFlow() : CSensor("Flow", CSenseType(FLOW, "Flow rate [m^3/s]"), CMeasType("L/s", "Flow", 1)){};
-	~CFlow();
-};
-class CPressure : public CSensor
-{
-public:
-	CPressure() : CSensor("Pressure", CSenseType(PREASURE, "Pressure [Pa]"), CMeasType("Pa", "Pressure", 1)){};
-	~CPressure();
-};
-class CMineral : public CSensor
-{
-public:
-	CMineral() : CSensor("Mineral", CSenseType(MINERAL, "Mineral concentration [mg/L]"), CMeasType("mg/L", "Mineral", 1)){};
-	~CMineral();
-};
-class CHumidity : public CSensor
-{
-public:
-	CHumidity() : CSensor("Humidity", CSenseType(HUMIDITY, "Humidity [%]"), CMeasType("%", "Humidity", 1)){};
-	~CHumidity();
-};
-class CTemperature : public CSensor
-{
-public:
-	CTemperature() : CSensor("Temperature", CSenseType(TEMPERATURE, "Temperature [C]"), CMeasType("C", "Temperature", 1)){};
-	~CTemperature();
-};
-class CRain : public CSensor
-{
-public:
-	CRain() : CSensor("Rain", CSenseType(RAIN, "Rain [mm]"), CMeasType("mm", "Rain", 1)){};
-	~CRain();
-};
-class CSolarIncidence : public CSensor
-{
-public:
-	CSolarIncidence() : CSensor("SolarIncidence", CSenseType(SOLAR_INCIDENCE, "Solar incidence [W/m^2]"), CMeasType("W/m^2", "Solar incidence", 1)){};
-	~CSolarIncidence();
-};
